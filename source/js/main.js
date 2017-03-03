7f82401b4c81451fd1971b3465b7fb91c6e4a569
Dima Zaytsev <zadima@gmail.com>
2017-03-03 17:17:03 +0300

    blur added

--- source/js/main.js
+++ source/js/main.js
@@ -1,31 +1,59 @@
-var parallax = (function () {
-  var bg = document.querySelector('.hero__bg');
-  var user = document.querySelector('.user');
-  var sectionText = document.querySelector('.hero__bg-img');
+var ScrollParallax = function () {
+    var layer1 = document.querySelector('.hero-parallax-layer-1');
+    var layer2 = document.querySelector('.hero-parallax-layer-2');
+    var layer3 = document.querySelector('.hero-parallax-layer-3');
+    var layer4 = document.querySelector('.hero-parallax-layer-4');
 
-  return {
-    move: function(block, windowScroll, strafeAmount) {
-      var strafe = windowScroll / -strafeAmount +'%';
-      var transformString = 'translate3d(0,' + strafe + '0)';
+    return {
+        move: function(block, windowScroll, strafeAmount) {
+            var strafe = windowScroll / -strafeAmount +'%';
+            var transformString = 'translate3d(0,' + strafe + ', 0)';
 
-      var style = block.style;
+            var style = block.style;
 
-      style.transform = transformString;
-      style.webkitTransform = transformString;
-    },
-      
-    init: function (wScroll) {
-        this.move(bg, wScroll, 45);
-        this.move(sectionText, wScroll, 20);
-        this.move(user, wScroll, 3);
+            style.transform = transformString;
+            style.webkitTransform = transformString;
+        },
+
+        init: function (wScroll) {
+            this.move(layer1, wScroll, 60);
+            this.move(layer2, wScroll, 30);
+            this.move(layer3, wScroll, 30);
+            this.move(layer4, wScroll, 25);
+        }
     }
-  }
-}());
+};
+
+var Blur = function () {
+    var wrapper = document.querySelector('.feedback'),
+        form = document.querySelector('.feedback__blur');
+
+
+    return {
+      set: function () {
+        var imgWidth = document.querySelector('.reviews__background-img').offsetWidth,
+            posLeft = -wrapper.offsetLeft,
+            posTop = -wrapper.offsetTop,
+            blurCss = form.style;
+
+        console.log(posLeft, posTop);
+        blurCss.backgroundSize = imgWidth + 'px' + ' ' + 'auto';
+        blurCss.backgroundPosition = posLeft + 'px' + ' ' + posTop + 'px';
+      }
+    }
+};
+
+var parallaxScroll = ScrollParallax();
+var blur = Blur();
+
+blur.set();
 
 window.onscroll = function() {
   var wScroll = window.pageYOffset;
 
-  parallax.init(wScroll);
+  parallaxScroll.init(wScroll);
+};
 
-  console.log(wScroll);
+window.onresize = function () {
+    blur.set();
 };
